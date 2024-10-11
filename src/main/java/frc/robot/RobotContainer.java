a2d679d84112a46937c16367ef4b67c982f0a305
jwt388 <jtnh388@gmail.com>
2024-10-11 16:13:09 -0400

    Add an option for tank drive.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -67,6 +68,8 @@ public class RobotContainer {
     // Default command is arcade drive. This will run unless another command
     // is scheduled over it.
     m_drivetrain.setDefaultCommand(getArcadeDriveCommand());
+    // m_drivetrain.setDefaultCommand(getTankDriveCommand());
+
 
     m_controller
     .x()
@@ -102,5 +105,15 @@ public class RobotContainer {
   public Command getArcadeDriveCommand() {
     return new ArcadeDrive(
         m_drivetrain, () -> -m_controller.getLeftY(), () -> -m_controller.getRightX());
+  }
+
+  /**
+   * Use this to get a tank drive command.
+   *
+   * @return the command to run in teleop
+   */
+  public Command getTankDriveCommand() {
+    return new RunCommand(
+        () -> m_drivetrain.tankDrive(-m_controller.getLeftY(), -m_controller.getRightY()), m_drivetrain);
   }
 }
